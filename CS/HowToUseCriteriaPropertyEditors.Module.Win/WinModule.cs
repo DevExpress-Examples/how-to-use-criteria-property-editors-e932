91ffd093ef4aed9f0ce173542e58ef024974dd9a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToUseCriteriaPropertyEditors.Module.Win/WinModule.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace HowToUseCriteriaPropertyEditors.Module.Win {
+    [ToolboxItemFilter("Xaf.Platform.Win")]
+    public sealed partial class HowToUseCriteriaPropertyEditorsWindowsFormsModule : ModuleBase {
+        public HowToUseCriteriaPropertyEditorsWindowsFormsModule() {
+            InitializeComponent();
+        }
+    }
+}
