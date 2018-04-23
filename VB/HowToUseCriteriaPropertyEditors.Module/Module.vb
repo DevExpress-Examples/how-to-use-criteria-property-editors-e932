22689b4098e8ae885405373919b481224b661b73
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/HowToUseCriteriaPropertyEditors.Module/Module.vb
@@ -0,0 +1,13 @@
+Imports DevExpress.ExpressApp
+Imports System.Reflection
+
+
+Namespace HowToUseCriteriaPropertyEditors.Module
+    Public NotInheritable Partial Class HowToUseCriteriaPropertyEditorsModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
